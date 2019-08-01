daebd96993cb386919d3781086b0030adbd82564
zpc <924824195@qq.com>
2019-08-01 12:05:47 +0200

    add list remove summary and CopyOnWrite Container summary

---
+++ JavaSummary/src/ListRemove/MutiThreadsIteratorException.java
@@ -0,0 +1,48 @@
+package ListRemove;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MutiThreadsIteratorException {
+    static List<String> list = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+
+        new Thread() {
+            public void run() {
+                Iterator<String> iterator = list.iterator();
+
+                while (iterator.hasNext()) {
+                    System.out.println(Thread.currentThread().getName() + ":"
+                        + iterator.next());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+        }.start();
+
+        new Thread() {
+            public synchronized void run() {
+                Iterator<String> iterator = list.iterator();
+
+                while (iterator.hasNext()) {
+                    String element = iterator.next();
+                    System.out.println(Thread.currentThread().getName() + ":"
+                        + element);
+                    if (element.equals("c")) {
+                        iterator.remove();
+                    }
+                }
+            };
+        }.start();
+
+    }
+}
